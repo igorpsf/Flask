d7404ddd5d999393b261c5ff4f8925f3df9a9e38
Igor Postnikov <igor.postnikov8@gmail.com>
2020-05-17 12:28:36 -0700

    Added add and rename functions

--- app.py
+++ app.py
@@ -37,6 +37,35 @@ def posts_detail(id):
     article = Article.query.get(id)
     return render_template('post_detail.html', article=article)
 
+@app.route('/posts/<int:id>/del')
+def posts_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "Error during post deleting"
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Error during article update"
+    else:
+
+        return render_template('post_update.html', article=article)
+
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == "POST":
@@ -50,7 +79,7 @@ def create_article():
             db.session.commit()
             return redirect('/posts')
         except:
-            return "Error during create article"
+            return "Error during article creation"
     else:
         return render_template('create-article.html')
 
